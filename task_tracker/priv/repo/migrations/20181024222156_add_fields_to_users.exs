f22d724db4b512aeb71ec92558eca9dc661eb9fb
Catu Berretta <catu@Catus-MacBook-Pro.local>
2018-10-24 20:36:58 -0400

    adding fields to users migration 2

--- task_tracker/priv/repo/migrations/20181024222156_add_fields_to_users.exs
+++ task_tracker/priv/repo/migrations/20181024222156_add_fields_to_users.exs
@@ -2,6 +2,9 @@ defmodule TaskTracker.Repo.Migrations.AddFieldsToUsers do
   use Ecto.Migration
 
   def change do
+    alter table(:users) do 
+      add :manager_id, references(:user_id, on_delete: :delete_all), default: nil
+    end
 
   end
 end
